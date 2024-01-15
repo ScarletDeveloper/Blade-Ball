744ae7e12e5ec675065820dc02a7115cd3e5f8f5
Wyrizon <108142718+ScarletDeveloper@users.noreply.github.com>
2024-01-15 22:42:57 +0100

    Add files via upload

---
+++ Blade Ball V2.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("Blade Ball V2")
+
+local KillingCheats = PhantomForcesWindow:NewSection("Closet")
+
+KillingCheats:CreateButton("Auto Parry", function()
+loadstring(game:HttpGet("https://scriptblox.com/raw/Blade-Ball-Opensource-autoparry-8123"))()
+end)
